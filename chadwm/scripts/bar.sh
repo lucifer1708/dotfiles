549f0263186297b9ccaec0304f501b6973672402
lucifer1708 <20bce091@nith.ac.in>
2023-06-20 20:27:51 +0530

    added changes

---
+++ chadwm/scripts/bar.sh
@@ -0,0 +1,60 @@
+#!/bin/dash
+
+# ^c$var^ = fg color
+# ^b$var^ = bg color
+
+interval=0
+
+# load colors
+. ~/.config/chadwm/scripts/bar_themes/dracula
+
+
+cpu() {
+  # cpu_val=$(grep -o "^[^ ]*" /proc/loadavg)
+  cpu_val=$(~/.local/bin/cpu-go)
+  printf "^c$black^ ^b$green^ CPU "
+  printf "^c$white^ ^b$grey^ $cpu_val "
+}
+
+temp() {
+  temp_value=$(sensors | awk '/^edge/ {print $2 }' | tr -d +)
+  printf "^c$black^ ^b$green^ TEMP"
+  printf "^c$white^ ^b$grey^ $temp_value"
+}
+
+battery() {
+  get_capacity="$(cat /sys/class/power_supply/BAT1/capacity)"
+  printf "^c$blue^   $get_capacity"
+}
+
+brightness() {
+  printf "^c$red^   "
+  printf "^c$red^%.0f\n" $(brightnessctl -m | cut -d, -f4 | tr -d %)
+}
+
+mem() {
+  printf "^c$blue^^b$black^  "
+  printf "^c$blue^ $(free -h | awk '/^Mem/ { print $3"/"$2 }' | sed s/i//g)"
+}
+
+wlan() {
+# run "conky -c $HOME/.config/chadwm/conky/system-overview"
+	case "$(cat /sys/class/net/wl*/operstate 2>/dev/null)" in
+	up) printf "^c$black^ ^b$blue^ 󰤨  ^d^%s" " ^c$blue^Connected" ;;
+	down) printf "^c$black^ ^b$blue^ 󰤭  ^d^%s" " ^c$blue^Disconnected" ;;
+	esac
+}
+
+clock() {
+	printf "^c$black^ ^b$darkblue^  "
+	printf "^c$black^^b$blue^ $(date '+%d/%m/%y %r')  "
+}
+
+
+while true; do
+
+  [ $interval = 0 ] || [ $(($interval % 3600)) = 0 ] 
+  interval=$((interval + 1))
+
+  sleep 2 && xsetroot -name " $(battery) $(brightness) $(cpu) $(temp) $(mem) $(wlan) $(clock)"
+done
---
+++ chadwm/scripts/run.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+xrdb merge ~/.Xresources 
+
+function run {
+ if ! pgrep $1 ;
+  then
+    $@&
+  fi
+}
+
+run "xrandr --output DP-0 --off --output DP-1 --off --output eDP-1-0 --mode 1920x1080 --pos 0x0 --rotate normal --output HDMI-A-1-0 --mode 1920x1080 --pos 1931x0 --rotate normal"
+run nm-applet
+run xfce4-power-manager
+run "/usr/lib/xfce4/notifyd/xfce4-notifyd"
+run "/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1"
+run picom 
+run "numlockx on"
+run "clipmenud"
+run "volumeicon"
+sxhkd -c ~/.config/chadwm/sxhkd/sxhkdrc &
+feh --bg-scale --randomize /home/lucifer/Pictures/walls/images/
+# feh --bg-scale --randomize ~/Pictures/walls/images/
+~/.config/chadwm/scripts/bar.sh &
+while type chadwm >/dev/null; do chadwm && continue || break; done
